9d534f47b449bdd3a12fc8459dd3082f92ca7aa5
volb <700679+volb@users.noreply.github.com>
2020-04-06 16:04:40 -0700

    Implement basic fetching

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const fetch = require('node-fetch');
 
 const base_url = "https://www.stands4.com/services/v2/poetry.php";
 const api_uid = "?uid=" + process.env.STANDS4_API_UID;
@@ -7,9 +8,12 @@ const full_base_url = base_url + api_uid + api_token;
 let term_query = "&term=";
 let data_format = "&format=json";
 
-exports.scrape = function() {
-
+exports.scrapePoem = function(query) {
+    fetch(full_base_url + term_query + query + data_format)
+    .then( r => r.json() )
+    .then( data => console.log(data) )
 }
 
 exports.translate = function () {
+    console.log("translation not implemented");
 }
